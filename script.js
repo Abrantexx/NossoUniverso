06a5a2331d4a4fc8dad1346702da0ec9328a5451
Abrantexx <abrantesthiago08@gmail.com>
2026-06-28 16:19:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+const card = document.querySelector(".card");
+const letter = document.getElementById("letter");
+const typedText = document.getElementById("typedText");
+const nextButton = document.getElementById("nextButton");
+const texto = `Oi, meu amor.
+
+Se você está lendo isso...
+
+é porque resolveu abrir um presente um pouco diferente.
+
+Você fez uma colagem para mim.
+
+E eu pensei:
+
+"Por que não fazer uma para você..."
+
+Só que do meu jeito.
+
+Então decidi transformar algumas linhas de código em uma lembrança.
+
+Espero que você goste.
+
+❤️`;
+function escreverTexto(){
+
+    let i = 0;
+
+    typedText.innerHTML = "";
+
+    function escrever(){
+
+        if(i < texto.length){
+
+            typedText.innerHTML += texto.charAt(i);
+
+            i++;
+
+            setTimeout(escrever,40);
+
+        }else{
+
+            nextButton.classList.remove("hidden");
+
+        }
+
+    }
+
+    escrever();
+
+}
 const hearts = document.getElementById("hearts");
 const music = document.getElementById("music");
 const startButton = document.getElementById("startButton");
@@ -30,6 +80,10 @@ startButton.addEventListener("click",()=>{
 
     music.play().catch(()=>{});
 
-    alert("A carta será aberta na V2 ❤️");
+    card.style.display = "none";
+
+    letter.classList.remove("hidden");
+
+    escreverTexto();
 
 });
